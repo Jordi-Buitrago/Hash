8cc5a6ca45ee12f44e08ce94293f6779d9a6623a
Jordi Buitrago <jordi.buitrago@mataro.epiaedu.cat>
2022-10-06 19:37:10 +0200

    new method creation and ecrypt class

---
+++ Hash/Encripta.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Security.Cryptography;
+using System.Runtime.CompilerServices;
+using static System.Net.Mime.MediaTypeNames;
+
+namespace Hash
+{
+    class Encripta
+    {
+        public Byte[] CreateHash (Byte[] text)
+        {
+            // Convertim l'string a un array de bytes
+            //byte[] bytesIn = UTF8Encoding.UTF8.GetBytes(text);
+            // Instanciar classe per fer hash
+            SHA512Managed SHA512 = new SHA512Managed();
+            // Calcular hash
+            byte[] hashResult = SHA512.ComputeHash(text);
+            // Si volem mostrar el hash per pantalla o guardar-lo en un arxiu de text
+            // cal convertir-lo a un string
+            String textOut = BitConverter.ToString(hashResult, 0);
+            Console.WriteLine("Hash {0}", textOut);
+            Console.ReadKey();
+            // Eliminem la classe instanciada
+            SHA512.Dispose();
+
+            return (text);
+
+        }   
+    }
+}
--- Hash/Program.cs
+++ Hash/Program.cs
@@ -13,26 +13,18 @@ namespace Hash
     {
         static void Main(string[] args)
         {
-           
-            Byte [] text = File.ReadAllBytes("hola.txt");
-            Console.WriteLine(Encoding.UTF8.GetString(text));
-            Console.ReadKey();
+            Encripta encripta = new Encripta();
+            Byte[] text = File.ReadAllBytes("hola.txt");
+            ReadDocument(text);
+            encripta.CreateHash(text);
+            
 
-            /*
 
-            // Convertim l'string a un array de bytes
-            //byte[] bytesIn = UTF8Encoding.UTF8.GetBytes(text);
-            // Instanciar classe per fer hash
-            SHA512Managed SHA512 = new SHA512Managed();
-            // Calcular hash
-            byte[] hashResult = SHA512.ComputeHash(bytesIn);
-            // Si volem mostrar el hash per pantalla o guardar-lo en un arxiu de text
-            // cal convertir-lo a un string
-            String textOut = BitConverter.ToString(hashResult, 0);
-            Console.WriteLine("Hash {0}", textOut);
-            Console.ReadKey();
-            // Eliminem la classe instanciada
-            SHA512.Dispose();*/
+            void ReadDocument(Byte[] text)
+            {
+                Console.WriteLine(Encoding.UTF8.GetString(text));
+                Console.ReadKey();
+            }
         }
     }
 }
