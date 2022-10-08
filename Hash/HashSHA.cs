8f8a2febc764aa087b207ee0969856c5f5b87489
Jordi Buitrago <jordibuimar@gmail.com>
2022-10-08 18:51:22 +0200

    update

---
+++ Hash/HashSHA.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Security.Cryptography;
+using System.Runtime.CompilerServices;
+using static System.Net.Mime.MediaTypeNames;
+using System.ComponentModel;
+
+namespace Hash
+{
+    class HashSHA
+    {
+
+        public String CreateHash (Byte[] text)
+        {
+            try
+            {
+                text = File.ReadAllBytes("hola.txt");
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine("Error al llegri l'arxiu " + e.Message);
+            }
+            // Convertim l'string a un array de bytes
+            byte[] bytesIn = text;
+            // Instanciar classe per fer hash
+            SHA512Managed SHA512 = new SHA512Managed();
+            // Calcular hash
+            byte[] hashResult = SHA512.ComputeHash(text);
+            // Si volem mostrar el hash per pantalla o guardar-lo en un arxiu de text
+            // cal convertir-lo a un string
+            String textOut = BitConverter.ToString(hashResult, 0);
+            
+            File.WriteAllBytes("hola.sha", hashResult);
+
+            Console.WriteLine("Hash creat correctament", textOut);
+
+
+            return textOut;
+        }
+        
+        public String HashVerify(Byte[] text, Byte[] hash)
+        {
+            // Convertim l'string a un array de bytes
+            byte[] bytesIn = text;
+            // Instanciar classe per fer hash
+            SHA512Managed SHA512 = new SHA512Managed();
+            // Calcular hash
+            byte[] hashResult = SHA512.ComputeHash(text);
+            // Si volem mostrar el hash per pantalla o guardar-lo en un arxiu de text
+            // cal convertir-lo a un string
+            String textOut = BitConverter.ToString(hashResult, 0);
+            String textOut2 = BitConverter.ToString(hash, 0);
+
+            if (textOut == textOut2)
+            {
+                Console.WriteLine("Hash verificat correctament", textOut);
+            }
+            else
+            {
+                Console.WriteLine("Hash no verificat", textOut);
+            }
+
+            return textOut;
+        }
+    } 
+}
--- Hash/Program.cs
+++ Hash/Program.cs
@@ -5,6 +5,7 @@ using System.Text;
 using System.Threading.Tasks;
 using System.Security.Cryptography;
 using System.Runtime.CompilerServices;
+using System.ComponentModel.Design;
 
 namespace Hash
 {
@@ -13,28 +14,53 @@ namespace Hash
     {
         static void Main(string[] args)
         {
-            Encripta encripta = new Encripta();
+            HashSHA hash = new HashSHA();
             Byte[] text = Encoding.UTF8.GetBytes("");
-            try
-            {
-                text = File.ReadAllBytes("hola.txt");
-                ReadDocument(text);
-            }
-            catch 
-            {
-                Console.WriteLine("Error al llegir el document");
-            }
             
-            Console.ReadKey();
-            encripta.CreateHash(text);
+            Console.WriteLine("Que vols fer?");
+            Console.WriteLine("1. Crear hash");
+            Console.WriteLine("2. Verificar hash");
+            Console.WriteLine("3. Sortir");
+
+            int menu = int.Parse(Console.ReadLine());
+
+            switch (menu)
+            {
+                case 1:
+                    try
+                    {
+                        text = File.ReadAllBytes("hola.txt");
+                        ReadDocument(text);
+                        Console.WriteLine(hash.CreateHash(text));
+                    }
+                    catch (Exception e)
+                    {
+                        Console.WriteLine("Error al llegri l'arxiu " + e.Message);
+                    }
+
+
+                case 2:
+                    
+                case 3:
+                    Environment.Exit(0);
+                    break;
+                default:
+                    Console.WriteLine("Opció incorrecta");
+                    break;
+            }
+
+            
+
             
 
 
             void ReadDocument(Byte[] text)
             {
                 Console.WriteLine(Encoding.UTF8.GetString(text));
-                Console.ReadKey();
+                
             }
+
+            
         }
     }
 }
